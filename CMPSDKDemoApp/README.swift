862d1da0175207ebc0171ad6992433739649d450
Fabio Torre <fabiotorre@gmail.com>
2025-02-18 11:31:54 +0100

    Updating README file

--- CMPSDKDemoApp/README.swift
+++ CMPSDKDemoApp/README.swift
@@ -5,5 +5,5 @@
 //  Created by Fabio Torre on 08/10/24.
 //
 
-// To run the app, please refer to the InitializationView class and inform
+// To run the app, please refer to the ConsentManager class and inform
 // your Code ID, fetched from your CMP configurations screen.
